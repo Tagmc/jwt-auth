1036a28af72770b5695ff717af30bc0c8f32c81c
Nguyễn Quang Huy <huymuchuoi@mac38s-MacBook-Pro.local>
2025-06-30 16:54:58 +0700

    config axios

--- client/src/utils/authorizedAxios.js
+++ client/src/utils/authorizedAxios.js
@@ -1,1 +1,44 @@
-// Author: EmHuyTapCode: https://youtube.com/@EmHuyTapCode
+import axios from "axios";
+import { toast } from "react-toastify";
+
+// Khởi tạo 1 đối tượng axios để config, custom axios
+
+let authorizedAxiosInstance = axios.create()
+
+// Thời gian chờ tối đa  của 1 request
+authorizedAxiosInstance.defaults.timeout = 1000 * 60 * 10
+
+// Cho phép axios tự động đính kèm và gửi cookie mỗi khi gửi request
+authorizedAxiosInstance.defaults.withCredentials = true 
+
+/**
+ * Cấu hình interceptor
+ * */ 
+
+// Add a request interceptor
+authorizedAxiosInstance.interceptors.request.use((config) => {
+    // Do something before request is sent
+    return config;
+  }, (error) => {
+    // Do something with request error
+
+
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+authorizedAxiosInstance.interceptors.response.use((response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    // Dùng toastify để hiển thị bất kể mọi mã lỗi lên màn hình  - Ngoại trừ mã 410 - GONE - phục vụ tự động refresh lại token
+    if (error.response?.status !== 410) {
+        toast.error(error.response?.data?.message || error?.message)
+    }
+    return Promise.reject(error);
+  });
+
+export default authorizedAxiosInstance
--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,26 +1,16 @@
-// Author: EmHuyTapCode: https://youtube.com/@EmHuyTapCode
+
 import { StatusCodes } from 'http-status-codes'
 import ms from 'ms'
+import { JwtProvider } from '~/providers/JwtProvider'
 
-/**
- * Mock nhanh thông tin user thay vì phải tạo Database rồi query.
- * Nếu muốn học kỹ và chuẩn chỉnh đầy đủ hơn thì xem Playlist này nhé:
- * https://www.youtube.com/playlist?list=PLP6tw4Zpj-RIMgUPYxhLBVCpaBs94D73V
- */
 const MOCK_DATABASE = {
   USER: {
     ID: 'EmHuyTapCode-sample-id-12345678',
-    EMAIL: 'EmHuyTapCode.official@gmail.com',
+    EMAIL: 'emhuytapcode@gmail.com',
     PASSWORD: 'EmHuyTapCode@123'
   }
 }
 
-/**
- * 2 cái chữ ký bí mật quan trọng trong dự án. Dành cho JWT - Jsonwebtokens
- * Lưu ý phải lưu vào biến môi trường ENV trong thực tế cho bảo mật.
- * Ở đây mình làm Demo thôi nên mới đặt biến const và giá trị random ngẫu nhiên trong code nhé.
- * Xem thêm về biến môi trường: https://youtu.be/Vgr3MWb7aOw
- */
 const ACCESS_TOKEN_SECRET_SIGNATURE = 'KBgJwUETt4HeVD05WaXXI9V3JnwCVP'
 const REFRESH_TOKEN_SECRET_SIGNATURE = 'fcCjhnpeopVn2Hg1jG75MUi62051yL'
 
@@ -32,8 +22,42 @@ const login = async (req, res) => {
     }
 
     // Trường hợp nhập đúng thông tin tài khoản, tạo token và trả về cho phía Client
+    //Tạo thông tin payload để đính kèm trong JWT token
+    const userInfo = {
+      id: MOCK_DATABASE.USER.ID,
+      email: MOCK_DATABASE.USER.EMAIL
+    }
 
-    res.status(StatusCodes.OK).json({ message: 'Login API success!' })
+    // Tạo 2 loại token
+    const accessToken = await JwtProvider.generateToken(userInfo, ACCESS_TOKEN_SECRET_SIGNATURE, '1h')
+    const refreshToken = await JwtProvider.generateToken(userInfo, REFRESH_TOKEN_SECRET_SIGNATURE, '14 days')
+    /**
+     * Xử lý trường hợp trả về http only cookie cho phía trình duyệt
+     * Về cái maxAge và thự viện ms: https://expressjs.com/en/api.html
+     * Đối với các maxAge - thời gian sống của cookie thì chúng ta sẽ để tối đa 14 ngày. thời gian sống cookie != thời gian sống token
+    */
+    console.log(accessToken)
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none', // domain fe != domain be,
+      maxAge: ms('14 days')
+    })
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none', // domain fe != domain be,
+      maxAge: ms('14 days')
+    })
+
+    // Trả về thông tin user cũng như sẽ trả về Tokens cho trường hợp phía FE cần lưu token vào localStorage
+
+    res.status(StatusCodes.OK).json({ 
+      ...userInfo,
+      accessToken,
+      refreshToken
+     })
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
   }
